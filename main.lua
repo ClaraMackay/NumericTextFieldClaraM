434f6ce74baa0901b79a3dc7338844890f1ca4e7
ClaraMackay <clara.mackay@ocsbstudent.ca>
2018-03-23 10:51:11 -0400

    changes

--- main.lua
+++ main.lua
@@ -4,6 +4,9 @@
 -- This program displays a math question and asks the
 -- user to answer in a numeric textfield. 
 ----------------------------------------------------------------------
+-- displays a question ands sets the colour 
+questionObject = display.newText("", display.contentWidth/4, display.contentHeight/2, "Georgia", 80)
+questionObject:setTextColor(255/255, 255/255, 255/255)
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
@@ -23,22 +26,51 @@ local numericField
 local randomNumber1
 local randomNumber2
 local userAnswer
-local incorrectAnswer
 local correctAnswer
 
+-- create more local variables
+local incorrectObject
+local pointsObject
+local points = 0
+local formula
 -----------------------------------------------------------------
 --LOCAL FUNCTIONS
 -----------------------------------------------------------------
 local function AskQuestion()
+end
 	-- generate 2 random nuymbers between a max. and a min. number
 	randomNumber1 = math.random(10, 20)
 	randomNumber2 = math.random(10, 20)
+
+	-- randomizes formulas from addition to multiplication to subtraction
+
+	formula = math.random(0, 2)
+
+if (fomula == 0) then
 
 	correctAnswer = randomNumber1 + randomNumber2
 
 	-- create question in text object
 	questionObject.text = randomNumber1 .. "+" .. randomNumber2 .. "="
+
+	elseif (formula == 1) then
+
+		-- calculates the correct answer
+		correctAnswer = randomNumber1 * randomNumber2
+
+		-- creates question in text object 
+		questionObject.text = randomNumber1 .. " x " .. randomNumber2 .. "="
+
+			elseif (formula == 1) then
+
+		-- calculates the correct answer
+		correctAnswer = randomNumber1 - randomNumber2
+
+		-- creates question in text object 
+		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. "="
+
 end
+
 
 local function HideCorrect()
 	correctObject.isVisible = false
@@ -65,6 +97,16 @@ function NumericFieldListener( event )
 			-- if the users answer and the correct answer are the same:
 			if (userAnswer == correctAnswer) then
 				correctObject.isVisible = true
+
+				--keeps track of the points 
+				points = points + 1 
+				pointsObject.text = "points = " .. points
+
+				-- call the HideCorrect function after 4 seconds
+				timer.performWithDelay(4000, HideCorrect)
+			else 
+				incorrectObject.isVisible = true 
+
 				timer.performWithDelay(4000, HideCorrect)
 
 			end
@@ -90,6 +132,9 @@ correctObject.isVisible = false
 incorrectObject = display.newText("Incorrect.", display.contentWidth/2, display.contentHeight*2/3, "Georgia", 80)
 incorrectObject:setTextColor(255/255, 255/255, 255/255)
 incorrectObject.isVisible = false
+
+-- displays numeber of points 
+pointsObject:setTextColor("points = 0.", display.contentWidth/2, display.contentHeight*2/3, "Georgia", 80)
 
 -- create numeric field
 numericField = native.newTextField(display.contentWidth/2, display.contentHeight/2, 150, 80)
